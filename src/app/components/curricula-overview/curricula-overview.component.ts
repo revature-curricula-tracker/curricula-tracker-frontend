b1b4246358f5f76965354eca219b764a1a60915a
Rashid Taleb <rashid@taleb.us>
2021-10-20 12:29:09 -0400

    created curricla view

---
+++ src/app/components/curricula-overview/curricula-overview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-curricula-overview',
+  templateUrl: './curricula-overview.component.html',
+  styleUrls: ['./curricula-overview.component.css']
+})
+export class CurriculaOverviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
