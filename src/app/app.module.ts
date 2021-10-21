ddcdc56832391166c4751ee4a6a2e8adc6f86a63
JoelWiegand <16wiegandjr@gmail.com>
2021-10-21 14:40:43 -0500

    change to table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CurriculaOverviewComponent } from './components/curricula-overview/curricula-overview.component';
-import { FullCalendarModule } from '@fullcalendar/angular';
-import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin!
-import interactionPlugin from '@fullcalendar/interaction';
-FullCalendarModule.registerPlugins([
-  dayGridPlugin,
-  interactionPlugin
-])
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,8 +10,7 @@ FullCalendarModule.registerPlugins([
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FullCalendarModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/curricula-overview/curricula-overview.component.ts
+++ src/app/components/curricula-overview/curricula-overview.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { CalendarOptions } from '@fullcalendar/angular';
 
 @Component({
   selector: 'app-curricula-overview',
@@ -7,18 +6,6 @@ import { CalendarOptions } from '@fullcalendar/angular';
   styleUrls: ['./curricula-overview.component.css']
 })
 export class CurriculaOverviewComponent implements OnInit {
-  calendarOptions:CalendarOptions = {
-    initialView:'dayGridMonth',
-    dateClick: this.handleDateClick.bind(this), // bind is important!
-    events: [
-      { title: 'event 1', date: '2021-10-04' ,backgroundColor:'green'},
-      { title: 'event 2', date: '2021-10-02' ,editable:true},
-      { title: 'event 2', date: '2021-10-02'  ,editable:true ,durationEditable:true},
-      { title: 'event 2', date: '2021-10-02' ,end:'2021-10-10'},
-      { title: 'event 2', date: '2021-10-02' ,interactive:true},
-      { title: 'event 4', date: '2021-10-22'}
-    ]
-  }
   title:string = "Curriculum Name";//name to be replaced by which curriculum it is
   tech: any[] = [];//array of tech for tech buttons
   topics : any[] = [];//array of topics and days they are on
@@ -27,7 +14,9 @@ export class CurriculaOverviewComponent implements OnInit {
   ngOnInit(): void {
   }
   handleDateClick(arg: { dateStr: string; }) {
-    alert('date click! ' + arg.dateStr)
+    //console.log('date click! ' + arg.dateStr)
   }
-
+  counter(i: number) {
+    return new Array(i);
+  }
 }
