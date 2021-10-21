a51355aec3d857559895b17ee6f7d389309b9846
JoelWiegand <16wiegandjr@gmail.com>
2021-10-21 11:37:19 -0500

    add basic layout and calender for curricula page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CurriculaOverviewComponent } from './components/curricula-overview/curricula-overview.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [{ path: '', component:CurriculaOverviewComponent}]
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CurriculaOverviewComponent } from './components/curricula-overview/curricula-overview.component';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin!
 
+FullCalendarModule.registerPlugins([
+  dayGridPlugin
+])
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +16,8 @@ import { CurriculaOverviewComponent } from './components/curricula-overview/curr
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FullCalendarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
