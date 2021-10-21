309151f0442e34e3f6f25d66007c87e5e90a51ac
Jacen <jbwyke@yahoo.com>
2021-10-21 12:59:23 -0700

    sonarcloud fixes

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,20 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
-import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { Component } from '@angular/core';
+import { faUserCircle, faPlus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
 
   faUserCircle = faUserCircle;
   faPlus = faPlus;
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
