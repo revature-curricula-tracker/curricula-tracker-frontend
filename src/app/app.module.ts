70ed4bb021976d93d4c33fbef2a2f329d00023a5
johnpham739 <johnpham739@gmail.com>
2021-10-21 11:47:17 -0500

    added navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbDropdownModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CurriculaOverviewComponent } from './components/curricula-overview/curricula-overview.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CurriculaOverviewComponent
+    CurriculaOverviewComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbDropdownModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
