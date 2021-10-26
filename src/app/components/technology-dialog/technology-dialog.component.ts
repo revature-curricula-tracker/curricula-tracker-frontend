0115b4a30f544b248c15b1de0b0095a14645e68a
sebas-vn <sebasvn2340@gmail.com>
2021-10-26 15:14:38 -0400

    update new tech dialog and table

--- src/app/components/technology-dialog/technology-dialog.component.ts
+++ src/app/components/technology-dialog/technology-dialog.component.ts
@@ -2,8 +2,8 @@ import {Component, Inject} from '@angular/core';
 import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 
 export interface DialogData {
-  animal: string;
   name: string;
+  technology: string;
 }
 
 @Component({
--- src/app/model/technology.ts
+++ src/app/model/technology.ts
@@ -2,9 +2,13 @@ import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog
 
 export class Technology {
 
+    id!: number;
+    name!: string;
+    color!: string;
 
-
-    constructor () {
-        
+    constructor (id: number, name: string, color: string) {
+        this.id = id;
+        this.name = name;
+        this.color = color;
     }
 }
