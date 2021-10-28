7b6ebd259b4234cda0d2ed91b2b2dd8c678fea5b
JoelWiegand <16wiegandjr@gmail.com>
2021-10-28 15:55:17 -0500

    fix code smell

--- src/app/services/curricula.service.ts
+++ src/app/services/curricula.service.ts
@@ -1,7 +1,7 @@
 import { TopicsForCurriculum } from './../model/topicsForCurriculum';
 import { Injectable } from '@angular/core';
 import { backendUrl } from './../../environments/environment';
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { Router } from '@angular/router';
 const url = `${backendUrl}/curriculumTopic`;
