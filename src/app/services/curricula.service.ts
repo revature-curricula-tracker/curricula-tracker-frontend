2b45962494ccfcfa14be18ee6002d76ffaa8c528
JoelWiegand <16wiegandjr@gmail.com>
2021-10-28 15:50:06 -0500

    add services

---
+++ src/app/services/curricula.service.ts
@@ -0,0 +1,21 @@
+import { TopicsForCurriculum } from './../model/topicsForCurriculum';
+import { Injectable } from '@angular/core';
+import { backendUrl } from './../../environments/environment';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Router } from '@angular/router';
+const url = `${backendUrl}/curriculumTopic`;
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CurriculaService {
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  //get all Curricula topics in the join table
+  public getAllTopicsForCurriculum(curriculumId: number): Observable<TopicsForCurriculum> {
+    return this.http.get<TopicsForCurriculum>(`${url}`);
+  }
+}
