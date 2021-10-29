bbc68f8af9d7fc6d2dd1d689275a2efe2c6417eb
Foxpope <48967406+Foxpope@users.noreply.github.com>
2021-10-29 16:33:49 -0400

    model updates to match updated erd

--- src/app/model/curriculum.ts
+++ src/app/model/curriculum.ts
@@ -4,16 +4,19 @@ export class Curriculum {
   curriculumName: string;
   numWeeks:number;
   numDays:number;
+  topics: Topic[];
 
   constructor( 
     curriculumId: number,
     curriculumName: string,
     numWeeks: number,
     numDays: number,
+    topics: Topic[],
   ) {
     this.curriculumId=curriculumId;
     this.curriculumName=curriculumName;
     this.numWeeks=numWeeks;
     this.numDays=numDays;
+    this.topics=topics
   }
 }
