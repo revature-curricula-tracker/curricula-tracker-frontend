119683b43cee041ceefb52910f4bdd1f9dca44f2
Noah Gaston <nrgman@tds.net>
2021-11-01 09:32:29 -0500

    spaces

--- src/app/services/topics.service.ts
+++ src/app/services/topics.service.ts
@@ -24,22 +24,22 @@ export class TopicsService {
 
   public findByName(name: string): Observable<Topic> {
     return this.http.get<Topic>(`${url}/search/${name}`)
-    .pipe( catchError( e => this.handleError('Find by name',e) ) );
+      .pipe( catchError( e => this.handleError('Find by name',e) ) );
   }
 
   public addTopic(top: Topic): Observable<Topic> {
     return this.http.post<Topic>(`${url}/add`,top)
-    .pipe( catchError( e => this.handleError('add a topic',e) ) );
+      .pipe( catchError( e => this.handleError('add a topic',e) ) );
   }
 
   public updateTopic(top: Topic): Observable<Topic> {
     return this.http.patch<Topic>(`${url}/${top.id}`,top)
-    .pipe( catchError( e => this.handleError('update a top',e) ) );
+      .pipe( catchError( e => this.handleError('update a top',e) ) );
   }
 
   public deleteTopic(top: Topic): Observable<boolean> {
     return this.http.delete<boolean>(`${url}/${top.id}`)
-    .pipe( catchError( e => this.handleError('delete a topic',e) ) );
+      .pipe( catchError( e => this.handleError('delete a topic',e) ) );
   }
   private handleError(action:string, httpError: HttpErrorResponse) {
     console.log("We have been trying to reach you regarding your car's extended waranty.");
